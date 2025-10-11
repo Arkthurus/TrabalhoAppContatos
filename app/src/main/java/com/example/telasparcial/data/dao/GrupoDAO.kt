b4c161bd1d235ecd06a46b68abb8519254b3134b
apenasolinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-10-10 23:00:03 -0300

    Adicionada a função buscarPeloNome ao GrupoDAO

--- app/src/main/java/com/example/telasparcial/data/dao/GrupoDAO.kt
+++ app/src/main/java/com/example/telasparcial/data/dao/GrupoDAO.kt
@@ -3,6 +3,7 @@ package com.example.telasparcial.data.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.telasparcial.data.entities.Grupo
 
 @Dao
@@ -13,5 +14,6 @@ interface GrupoDAO {
     @Delete
     suspend fun deletarGrupo(grupo: Grupo)
 
-
+    @Query("SELECT * FROM Grupos WHERE nome = :nome LIMIT 1")
+    suspend fun buscarPeloNome(nome: String): Grupo?
 }
