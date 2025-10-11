a9ec116a3a4050bad98b94b6cd27519e5ffde9cf
apenasolinco <120327456+ApenasOLinco@users.noreply.github.com>
2025-10-10 22:55:57 -0300

    Criado o grupo padrão "Favoritos" na inicialização do banco de dados

--- app/src/main/java/com/example/telasparcial/data/AppDataBase.kt
+++ app/src/main/java/com/example/telasparcial/data/AppDataBase.kt
@@ -4,23 +4,29 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.SQLiteConnection
 import com.example.telasparcial.data.dao.ContatosDAO
 import com.example.telasparcial.data.dao.GrupoContatoDAO
+import com.example.telasparcial.data.dao.GrupoDAO
 import com.example.telasparcial.data.entities.Contato
 import com.example.telasparcial.data.entities.Grupo
 import com.example.telasparcial.data.entities.GrupoContato
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(
     entities = [
         Contato::class,
         Grupo::class,
         GrupoContato::class
-    ], version = 3
+    ], version = 4
 )
 abstract class AppDataBase : RoomDatabase() {
 
     abstract fun contatosDao(): ContatosDAO
     abstract fun grupoContatoDao(): GrupoContatoDAO
+    abstract fun grupoDao(): GrupoDAO
 
     companion object {
         @Volatile
@@ -40,6 +46,12 @@ abstract class AppDataBase : RoomDatabase() {
                         .fallbackToDestructiveMigration(true)
                         .build()
                     INSTANCE = instance
+
+                    // Cria o grupo "Favoritos"
+                    CoroutineScope(Dispatchers.IO).launch {
+                        instance.grupoDao().inserirGrupo(Grupo(nome = "Favoritos"))
+                    }
+
                     return instance
                 }
             }
